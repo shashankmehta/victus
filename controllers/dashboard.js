cef69ec1da413e74e0f124d3d979d0a501a89596
Ashwini Khare <khare.ashwini@gmail.com>
2014-06-07 17:06:34 +0530

    Dashboard content rendered through views

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,5 +3,7 @@
  * Dummy response to demonstrate
 **/
 exports.getDashboard = function(req,res){
-	res.json({ "hello" : "world" })
+	res.render('dashboard/index',{
+		title : 'Dashboard - My Restaurant'
+	})
 };
